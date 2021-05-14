5a389133873fa8e959e7d0fec86c3c19a8db27ec
Bruno Andre <brunoandre171@gmail.com>
2021-05-14 03:31:02 -0300

    Corrigindo erro no settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    
 ]
 
 MIDDLEWARE = [
@@ -120,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static)')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
